41b5eb859cb4041882edacaa33e4c2605a197213
Ayush <unitedestates.ayush10@gmail.com>
2023-09-09 22:31:31 +0530

    primary routing done

---
+++ client/src/components/dashborard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const dashborard = () => {
+  return <div>dashborard</div>;
+};
+
+export default dashborard;
---
+++ client/src/components/login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const login = () => {
+  return <div>workking</div>;
+};
+
+export default login;
