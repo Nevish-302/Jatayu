b616019235f8094e5301adfb43bfc938c5e98242
Ayush <unitedestates.ayush10@gmail.com>
2023-09-09 16:37:02 +0530

    higher order container

---
+++ client/src/hoc/SectionWrapper.jsx
@@ -0,0 +1,20 @@
+import { styles } from "../styles";
+
+const StarWrapper = (Component, idName) =>
+  function HOC() {
+    return (
+      <section
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+        {/* <span className="hash-span" id={idName}>
+          &nbsp;
+        </span> */}
+
+        <Component />
+      </section>
+    );
+  };
+
+export default StarWrapper;
