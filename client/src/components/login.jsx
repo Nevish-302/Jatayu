e4bb4c8f20221390ffbf495640a138865fe54ff7
Ayush <unitedestates.ayush10@gmail.com>
2023-09-13 01:31:35 +0530

    login form layout

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,38 +1,74 @@
+// LoginForm.js
 import React from "react";
-import { styles } from "../styles";
-import Navbar from "../components/navbar";
-import image from "../assets/image/bg1.png";
-const login = () => {
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { RiEyeLine, RiEyeCloseLine } from "react-icons/ri";
+
+const initialValues = {
+  organizationId: "",
+  password: "",
+};
+
+const validationSchema = Yup.object().shape({
+  organizationId: Yup.string().required("Organization ID is required"),
+  password: Yup.string().required("Password is required"),
+});
+
+const LoginForm = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <>
-      <div className={`${styles.paddingX}`}>
-        <Navbar />
-        <div class="flex flex-col sm:flex-row justify-between items-center p-4">
-          <div class="flex-1 pr-0 md:pr-9 md:justify-start">
-            <img src={image} alt="Image" class="  w-5/6  mx-auto" />
-          </div>
-          <div class="flex-1 border-2  border-prime-blue justify-end h-full">
-            <form className="h-full">
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-              <div>LOGIN</div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </>
+    <div>
+      <h2>Log In</h2>
+      <p>To login, enter your credentials</p>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+          // Handle form submission here
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div>
+              <label htmlFor="organizationId">Organization ID</label>
+              <Field type="text" id="organizationId" name="organizationId" />
+              <ErrorMessage
+                name="organizationId"
+                component="div"
+                className="error"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <div className="password-field">
+                <Field
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  name="password"
+                />
+                <span onClick={togglePasswordVisibility}>
+                  {showPassword ? <RiEyeCloseLine /> : <RiEyeLine />}
+                </span>
+              </div>
+              <ErrorMessage name="password" component="div" className="error" />
+            </div>
+            <div>
+              <button type="submit" disabled={isSubmitting}>
+                Log In
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+      <p>Don't have an account? Sign up</p>
+    </div>
   );
 };
 
-export default login;
+export default LoginForm;
