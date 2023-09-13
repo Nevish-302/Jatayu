ab24ecce5232d6e20c2929b8f847d5c04e42d9b0
Nevish-302 <shiven302@gmail.com>
2023-09-13 15:33:23 +0530

    RoutesChanged

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const employeeRouter = require('./routes/employeeRoutes');
-// const organisationRouter = require('./routes/organisationRoutes');
+const organisationRouter = require('./routes/organisationRoutes');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.use('/api/v1/employee',employeeRouter);
-// app.use('/api/v1/organisation',organisationRouter);
+app.use('/api/v1/organisation',organisationRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
