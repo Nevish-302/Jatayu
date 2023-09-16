aa43a005c02e23cb34f857dd5eee8b994768e134
Nevish-302 <shiven302@gmail.com>
2023-09-16 16:08:36 +0530

    merge

--- server/routes/organisationRoutes.js
+++ server/routes/organisationRoutes.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const organisationController = require('../controllers/organisationController');
 const requestController = require('../controllers/requestController');
-<<<<<<< HEAD
-const SessionController = require('../controllers/sessionController')
 const teamController = require('../controllers/teamController')
-=======
 const sessionController = require('../controllers/sessionController')
->>>>>>> 253d2b4c35ab91057c535f74f68130631314e54a
 const OauthController = require('../controllers/OauthController');
 const { protect } = require('../controllers/OauthController');
 
@@ -25,21 +21,16 @@ router.patch('/updateOrganisation', organisationController.updateOrganisation);
 router.get('/getOrganisation/:id', organisationController.getOrganisation);
 router.get('/getAllOrganisationsBySession', organisationController.getAllOrganisationBySession);
 router.get('/getAllOrganisations', organisationController.getAllOrganisation);
+
+// is this function not working?
 // router.post('/acceptRequestFromOff', organisationController.AcceptReqFromOff);
 router.delete('/deleteOrganisation/:id', organisationController.deleteOrganisation);
-<<<<<<< HEAD
-router.get('/getAllRequestsBySession', SessionController.getAllRequests)
-router.post('/createSession', SessionController.createSession)
-router.post('/addOrganisation', SessionController.addOrganisation)
-router.route('/teamCreate').post(teamController.createTeam)	
-router.route('/addToTeam').post(teamController.addEmployee)
-=======
 router.get('/getAllRequestsBySession', sessionController.getAllRequests)
 router.post('/createSession', sessionController.createSession)
 router.post('/addOrganisation', sessionController.addOrganisation)
+router.route('/teamCreate').post(teamController.createTeam)	
+router.route('/addToTeam').post(teamController.addEmployee)
 router.get('/sessions/:sessionId', sessionController.getSessionById);
 router.get('/sessions/byOrganisation/:organisationId', sessionController.getSessionsByOrganisationId);
-
->>>>>>> 253d2b4c35ab91057c535f74f68130631314e54a
 
 module.exports = router;
