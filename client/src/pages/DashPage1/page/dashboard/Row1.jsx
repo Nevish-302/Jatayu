91ca49cecd639ab8946ae0488050c57907525bed
Ayush <unitedestates.ayush10@gmail.com>
2023-09-19 03:47:57 +0530

    dash styling +2

--- client/src/pages/DashPage1/page/dashboard/Row1.jsx
+++ client/src/pages/DashPage1/page/dashboard/Row1.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Stack, useTheme } from "@mui/material";
-import Card from "./Card"; // Make sure to use the correct filename
+import Card from "./card"; // Make sure to use the correct filename
 
 const Row1 = () => {
   const theme = useTheme();
@@ -11,25 +11,8 @@ const Row1 = () => {
       gap={1}
       justifyContent={{ xs: "center", sm: "space-between" }}
     >
-      <Card
-        title={"Total Casualties :"}
-        subtitle={"54"}
-      />
       <Card title={"Total Casualties :"} subtitle={"54"} />
 
-      <Card
-        title={"Total injured : "}
-        subtitle={"128"}
-      />
-
-      <Card
-        title={"Total Survivors :"}
-        subtitle={"540"}
-      />
-      <Card
-        title={"Estimated Affectees :"}
-        subtitle={"1500+"}
-      />
       <Card title={"Total injured : "} subtitle={"128"} />
 
       <Card title={"Total Survivors :"} subtitle={"540"} />
