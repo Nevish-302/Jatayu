859ed24f845c2f33c44725f27d8a2c03d67088a5
Ayush <unitedestates.ayush10@gmail.com>
2023-09-19 02:48:29 +0530

    dash styling +1

--- client/src/pages/DashPage1/page/dashboard/card.jsx
+++ client/src/pages/DashPage1/page/dashboard/card.jsx
@@ -1,173 +1,27 @@
 import React from "react";
-import { Box, Paper, Stack, Typography, useTheme } from "@mui/material";
-import { ResponsivePie } from "@nivo/pie";
+import { Card as MUICard, CardContent, Typography } from "@mui/material";
+import { styled } from "@mui/system";
 
-const Card = ({ icon, title, subTitle, increase, data }) => {
-  const theme = useTheme();
+const StyledCard = styled(MUICard)(({ theme }) => ({
+  width: 345,
+  height: 120,
+}));
 
-  // Define a custom color scheme
-  const customColorScheme = {
-    colors: ["#0074D9", "#5BC0DE", "#89C4F4"],
-  };
-
+function Card({ title, subtitle }) {
   return (
-    <Paper
-      sx={{
-        flexGrow: 1,
-        minWidth: "333px",
-        p: 1.5,
-        display: "flex",
-        justifyContent: "space-between",
-      }}
-    >
-      <Stack gap={1}>
-        {icon}
-        <Typography variant="body2" sx={{ fontSize: "13px" }}>
+    <StyledCard>
+      <CardContent>
+        <Typography variant="h5" component="div">
           {title}
         </Typography>
-        <Typography variant="body2" sx={{ fontSize: "13px" }}>
-          {subTitle}
+        <br />
+        <Typography variant="body2" color="textSecondary">
+          {subtitle}
         </Typography>
-      </Stack>
-
-      <Stack alignItems={"center"}>
-        <Box height={"70px"} width={"87px"}>
-          <ResponsivePie
-            data={data}
-            margin={{ top: 10, right: 0, bottom: 10, left: 0 }}
-            innerRadius={0.7}
-            theme={{
-              textColor: theme.palette.text.primary,
-              fontSize: 11,
-              axis: {
-                domain: {
-                  line: {
-                    stroke: theme.palette.divider,
-                    strokeWidth: 1,
-                  },
-                },
-                legend: {
-                  text: {
-                    fontSize: 12,
-                    fill: theme.palette.text.primary,
-                  },
-                },
-                ticks: {
-                  line: {
-                    stroke: theme.palette.divider,
-                    strokeWidth: 1,
-                  },
-                  text: {
-                    fontSize: 11,
-                    fill: theme.palette.text.secondary,
-                  },
-                },
-              },
-              grid: {
-                line: {
-                  stroke: theme.palette.divider,
-                  strokeWidth: 1,
-                },
-              },
-              legends: {
-                title: {
-                  text: {
-                    fontSize: 11,
-                    fill: theme.palette.text.primary,
-                  },
-                },
-                text: {
-                  fontSize: 11,
-                  fill: theme.palette.text.primary,
-                },
-                ticks: {
-                  line: {},
-                  text: {
-                    fontSize: 10,
-                    fill: theme.palette.text.primary,
-                  },
-                },
-              },
-              annotations: {
-                text: {
-                  fontSize: 13,
-                  fill: theme.palette.text.primary,
-                  outlineWidth: 2,
-                  outlineColor: "#ffffff",
-                  outlineOpacity: 1,
-                },
-                link: {
-                  stroke: "#000000",
-                  strokeWidth: 1,
-                  outlineWidth: 2,
-                  outlineColor: "#ffffff",
-                  outlineOpacity: 1,
-                },
-                outline: {
-                  stroke: "#000000",
-                  strokeWidth: 2,
-                  outlineWidth: 2,
-                  outlineColor: "#ffffff",
-                  outlineOpacity: 1,
-                },
-                symbol: {
-                  fill: "#000000",
-                  outlineWidth: 2,
-                  outlineColor: "#ffffff",
-                  outlineOpacity: 1,
-                },
-              },
-              tooltip: {
-                container: {
-                  background: theme.palette.background.default,
-                  color: theme.palette.text.primary,
-                  fontSize: 12,
-                },
-                basic: {},
-                chip: {},
-                table: {},
-                tableCell: {},
-                tableCellValue: {},
-              },
-            }}
-            enableArcLabels={false}
-            enableArcLinkLabels={false}
-            padAngle={0.7}
-            cornerRadius={3}
-            activeOuterRadiusOffset={8}
-            borderWidth={1}
-            borderColor={{
-              from: "color",
-              modifiers: [["darker", 0.2]],
-            }}
-            defs={[
-              {
-                id: "dots",
-                type: "patternDots",
-                background: "inherit",
-                color: "rgba(255, 255, 255, 0.3)",
-                size: 4,
-                padding: 1,
-                stagger: true,
-              },
-              {
-                id: "lines",
-                type: "patternLines",
-                background: "inherit",
-                color: "rgba(255, 255, 255, 0.3)",
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10,
-              },
-            ]}
-            // Pass the custom color scheme
-            colors={customColorScheme.colors}
-          />
-        </Box>
-        <Typography variant="body2">{increase}</Typography>
-      </Stack>
-    </Paper>
+        <br />
+      </CardContent>
+    </StyledCard>
   );
-};
+}
 
 export default Card;
